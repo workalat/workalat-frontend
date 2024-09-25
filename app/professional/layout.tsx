23c6f5a6a8c47e79c25cb2181473be43edcd388b
jayeedwebdevloper <jayeedgix@gmail.com>
2024-09-25 18:09:10 +0600

    upload

--- app/professional/layout.tsx
+++ app/professional/layout.tsx
@@ -1,23 +1,19 @@
 "use client"
 import { ReactNode } from 'react'
 
-import { Navbar } from '@/components/navbar/navbar'
-import { useUserContext } from '@/context/user_context'
 import AuthNavbar from '@/components/navbar/auth_navbar';
 
 export default function ProfessionalLayout(
-    { children } :
+  { children }:
     { children: ReactNode }
 ) {
-    
-    const { user } = useUserContext();
-    
+
+
   return (
     <>
-        {
-            user ? <AuthNavbar /> : <Navbar mode="light" />
-        }
-        {children}
+      <AuthNavbar />
+
+      {children}
     </>
   )
 }
