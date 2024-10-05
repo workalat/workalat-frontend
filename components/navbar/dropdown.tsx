7258c05529c5143db6616c54a66c84a78be4267e
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2024-10-05 16:11:04 +0500

    fix categories

--- components/navbar/dropdown.tsx
+++ components/navbar/dropdown.tsx
@@ -1,4 +1,7 @@
+"use client";
 import Link from "next/link";
+import { IconButton } from "@mui/material";
+import { useState } from "react";
 
 import businessIcon from "@/public/icons/business.svg";
 import eventsIcon from "@/public/icons/events.svg";
@@ -13,28 +16,33 @@ import gardeningIcon from "@/public/icons/gardening.svg";
 import investigatorsIcon from "@/public/icons/investigators.svg";
 import coachIcon from "@/public/icons/coach.svg";
 import arrowFilledIcon from "@/public/icons/arrow_filled.svg";
+import PlaceRequestModal from "../Clients/PlaceRequestModal/PlaceRequestModal";
 
 const group1 = [
-  { link: "/", label: "Business", icon: businessIcon },
-  { link: "/", label: "Events & Entertainers", icon: eventsIcon },
-  { link: "/", label: "Health & Wellness", icon: healthIcon },
-  { link: "/", label: "House & Home", icon: houseIcon },
-  { link: "/", label: "Lessons & Training", icon: lessonsIcon },
-  { link: "/", label: "More", icon: moreIcon },
+  { link: "/", label: "Web Design", icon: businessIcon },
+  { link: "/", label: "Graphic Design", icon: eventsIcon },
+  { link: "/", label: "Accounting", icon: healthIcon },
+  { link: "/", label: "SEO", icon: houseIcon },
+  { link: "/", label: "Social Media Marketing", icon: lessonsIcon },
+  { link: "/", label: "Music Band", icon: moreIcon },
 ];
 
 const group2 = [
   [
-    { link: "/", label: "Pets", icon: petIcon },
-    { link: "/", label: "Cleaning", icon: cleaningIcon },
-    { link: "/", label: "Web Development", icon: webIcon },
-    { link: "/", label: "Gardening", icon: gardeningIcon },
-    { link: "/", label: "Private Investigators", icon: investigatorsIcon },
-    { link: "/", label: "Life Coach", icon: coachIcon },
+    { link: "/", label: "Dog & Pet Grooming", icon: petIcon },
+    { link: "/", label: "House Cleaning", icon: cleaningIcon },
+    { link: "/", label: "Bricklayer", icon: webIcon },
+    { link: "/", label: "Painting", icon: gardeningIcon },
+    { link: "/", label: "Bodyguard", icon: investigatorsIcon },
+    { link: "/", label: "Home Care", icon: coachIcon },
   ],
 ];
 
 const Dropdown = ({ className }: { className: string }) => {
+  const [openModal, setOpenModal] = useState(false);
+  const handleOpenModal = () => setOpenModal(true);
+  const handleCloseModal = () => setOpenModal(false);
+
   return (
     <div
       className={`${className} absolute top-full py-6 px-5 bg-white shadow-lg max-w-max rounded-md flex justify-center items-center divide-x-1 gap-10`}
@@ -59,7 +67,9 @@ const Dropdown = ({ className }: { className: string }) => {
                 </span>
                 <p>{item.label}</p>
               </span>
-              <img alt="" src={arrowFilledIcon.src} />
+              <IconButton onClick={handleOpenModal}>
+                <img alt="" src={arrowFilledIcon.src} />
+              </IconButton>
             </Link>
           ))}
         </div>
@@ -86,13 +96,16 @@ const Dropdown = ({ className }: { className: string }) => {
                     </span>
                     <p>{item.label}</p>
                   </span>
-                  <img alt="" src={arrowFilledIcon.src} />
+                  <IconButton onClick={handleOpenModal}>
+                    <img alt="" src={arrowFilledIcon.src} />
+                  </IconButton>
                 </Link>
               ))}
             </div>
           ))}
         </div>
       </div>
+      <PlaceRequestModal open={openModal} onClose={handleCloseModal} />
     </div>
   );
 };
